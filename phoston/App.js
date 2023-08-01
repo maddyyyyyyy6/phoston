8c1815b5865b6ccb9dccfb68e1876bc104f63580
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-08-01 19:18:28 +0000

    font

--- phoston/App.js
+++ phoston/App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Base from './app/screens/Base';
-
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Base />
